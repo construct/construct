487ec3a252600b7ba089725119a6c4c0ae4a222a
M. Domenzain <ld@airinov.fr>
2017-03-22 14:54:51 +0100

    Correct palindrome example
    This was still wrong. Now the entire session was copy-pasted.

--- construct/core.py
+++ construct/core.py
@@ -1130,11 +1130,13 @@ class RepeatUntil(Subconstruct):
         >>> # Parse bytes until five even ones have appeared
         >>> RepeatUntil(lambda x,lst,ctx: sum(map(lambda e: e % 2 == 0, lst)) > 5, Byte).parse(b"\x01\x02\x03\x04\x05\x06\x07\x08\x09")
         [0, 1, 2, 3, 4, 5, 6, 7, 8]
-        >>> # Parse until the sequence is a palindrome of at least seven Bytes.
-        >>> RepeatUntil(lambda x,lst,ctx: (lst[::-1] == lst) and (len(lst) >= 7), Byte).parse(b"\x01\x02\x03\x04\x03\x02\x01\x00")
+        >>> # Parse until the sequence is a palindrome of at least three Bytes.
+        >>> 3pal = cs.RepeatUntil(lambda x,xs,ctx: (xs[::-1] == xs) and (len(xs) > 3), cs.Byte)
+        >>> 3pal.parse(b'\x01\x02\x03\x04\x03\x02\x01\x00')
         [1, 2, 3, 4, 3, 2, 1]
-        >>> RepeatUntil(lambda x,lst,ctx: (lst[::-1] == lst) and (len(lst) >= 7), Byte).build(_)
-        b'\x01\x02\x03\x04\x03\x02\x01\x00'
+        >>> 3pal.build(_)
+        '\x01\x02\x03\x04\x03\x02\x01'
+
     """
     __slots__ = ["predicate"]
     def __init__(self, predicate, subcon):
